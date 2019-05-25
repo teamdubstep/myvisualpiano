c1834bbbbb80bb41a93ff5abdd349a68a95ec8d6
Parker Reno Ciambrone <apps@parkerreno.net>
2019-05-24 23:37:41 -0700

    Added reset after playback

--- GazePianoPrototype/PianoPage.xaml.cs
+++ GazePianoPrototype/PianoPage.xaml.cs
@@ -529,7 +529,7 @@
                     break;
                 case RecordingStatus.Recorded:
                     //TODO: Play recording
-                    this.playbackTimer = new Timer(100);
+                    this.playbackTimer = new Timer(100); // Time in ms between timer ticks
                     this.playbackTimer.Elapsed += PlaybackTimer_Elapsed;
                     this.playbackTimer.Start();
                     this.playbackStart = DateTime.Now;
@@ -539,7 +539,7 @@
             }          
         }
 
-        private void PlaybackTimer_Elapsed(object sender, ElapsedEventArgs e)
+        private async void PlaybackTimer_Elapsed(object sender, ElapsedEventArgs e)
         {
             TimeSpan currentPosition = DateTime.Now - this.playbackStart;
             IEnumerable<RecordingItem> toPlay = this.recording.RecordingItems.Where(x => !x.Played && x.Timecode < currentPosition);
@@ -547,6 +547,19 @@
             {
                 this.SendNoteToSynth(item.MidiMessage);
                 item.Played = true;
+            }
+
+            // If all items have been played, reset
+            if (!this.recording.RecordingItems.Any(x => !x.Played))
+            {
+                this.playbackTimer.Stop();
+                this.playbackTimer.Dispose();
+                this.recording.ResetPlayedStatus();
+                this.recordingStatus = RecordingStatus.Recorded;
+                await this.Dispatcher.RunAsync(Windows.UI.Core.CoreDispatcherPriority.Normal, () =>
+                {
+                    this.RecordLabel.Glyph = "\uE768"; // Play icon
+                });                
             }
         }
     }
